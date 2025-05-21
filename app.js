f31ecc65273e4d337a43b519f878e52c5de05fab
Abdullo-dev-dev <abdullou272@gmail.com>
2025-05-21 16:35:31 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+ let yosh = prompt('yoshingizni kiriting');
+ const ism = prompt('ismingizni kiriting');
+ const jinsinhiz = prompt('jinsingizni kiriting');
+ const qayerliksiz = prompt('qayerliksiz');
+ const familyangiz = prompt('familiyangizni kiriitng');
+ const yilingiz = prompt('nechinchi yilsiz');
+ const rangiz = prompt('korinishizni rangni kiriting');
+ let boyiz = prompt('boyingizni uzunligi nechi');
+ let kiloyiz = prompt('nechi kilosiz');
+ let razmeriz = prompt('oyogingizni razmeri')
+
+ console.log(`mani ismim ${ism} man ${yosh} yoshdaman man ${qayerliksiz} likman mani familiyam ${familyangiz} man ${jinsinhiz} bolaman man ${yilingiz} yilman mani korinishimi rangi ${rangiz} mani boyimi razmeri ${boyiz} metr man ${kiloyiz} kiloman mani oyogimi razmeri ${razmeriz}`  )
+ 
